c62418b1542bedf3455166be2d846f7767879435
nguyendt-tn <nguyendt@ptd.edu.vn>
2020-08-07 20:26:36 +0700

    update

--- vendor/js/main.js
+++ vendor/js/main.js
@@ -3,7 +3,7 @@ const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 nn.addEventListener('click',()=>{nn.classList.toggle('active');mobile_menu.classList.toggle('active');});
-document.addEventListener('scroll',()=>{var scroll_position = window.scrollY;if(scroll_position > 250){header.style.backgroundColor = '#137ef6';}else{header.style.backgroundColor='transparent';}});
+document.addEventListener('scroll',()=>{var scroll_position = window.scrollY;if(scroll_position > 250){header.style.backgroundColor = '#00baff';}else{header.style.backgroundColor='transparent';}});
 menu_item.forEach((item)=>{item.addEventListener('click',()=>{nn.classList.toggle('active');mobile_menu.classList.toggle('active');});});
 function openSearch() {(document.getElementById("search-over").style.display = "block");}
 function closeSearch() {(document.getElementById("search-over").style.display = "none");}
